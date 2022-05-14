014670f91a27e06150dd0291d1c18f2b330f7880
Rahul Mittal <rahulmittal499@gmail.com>
2022-05-15 01:08:19 +0530

    Added CRUD APIs

--- app.js
+++ app.js
@@ -8,7 +8,10 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.use('/tasks', taskRoutes);
-
+app.use('/',(req, res)=>{
+    res.status(404);
+    res.json({message: 'Invalid'})
+})
 
 const PORT = 5000;
 app.listen(PORT);
